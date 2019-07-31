21f0d8798f2c4ef362112d78325e18ea5b4b3e17
valiant.zhao <287730669@qq.com>
2019-07-31 22:23:03 +0800

    flask template

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello,World!您好！"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [
+        {
+            'author': {'nickname': 'Vz'},
+            'body': 'test'
+        },
+        {
+            'author': {'nickname': 'bbbb'},
+            'body': 'ffff'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 if __name__ == '__main__':
