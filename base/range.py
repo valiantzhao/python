65d737d7fea10265a8f935991471cb5744fdfc2f
valiant.zhao <287730669@qq.com>
2019-08-07 23:04:14 +0800

    range

---
+++ base/range.py
@@ -0,0 +1,20 @@
+for value in range(1, 6):
+    print(value)
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+squares = []
+for value in range(1, 11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+squares1 = [value**2 for value in range(1, 11)]
+print(squares1)
